8334de3051659fbeef99ca1311e4f245fa055aa8
Sajad Jalilian <sajadjalilian88@gmail.com>
2020-01-31 19:12:58 +0330

    working views adding item_detail.html adding todo.detail.html refactorin views.py

--- todolist/views.py
+++ todolist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
@@ -7,22 +7,28 @@ from .models import Todo, List, Item
 
 
 def todo_list(request):
-    # return HttpResponse("You're looking at todo_list")
     latest_todos = Todo.objects.order_by('-pub_date')
     context = {'todos': latest_todos}
     return render(request, 'todolist/todo_list.html', context)
 
 
 def todo(request, todo_id):
-    return HttpResponse("You're looking at todo %s." % todo_id)
+    todo = get_object_or_404(Todo, pk=todo_id)
+    context = {'todo': todo}
+    return render(request, 'todolist/todo_detail.html', context)
 
 
 def add_todo(request):
     return HttpResponse("You're looking at add_todo")
 
 
-def item(request, item_id, todo_id):
-    return HttpResponse(f"You're looking at item {item_id} and todo {todo_id}")
+def item(request, todo_id, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+    todo = get_object_or_404(Todo, pk=todo_id)
+    context = {'item': item, 'todo': todo}
+    return render(request, 'todolist/item_detail.html', context)
+
+    # return HttpResponse(f"You're looking at item {item_id} and todo {todo_id}")
 
 
 def add_item(request):
