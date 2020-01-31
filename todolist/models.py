123ee296a20414a119819ead6df43db01bd3edaa
Sajad Jalilian <sajadjalilian88@gmail.com>
2020-01-31 16:24:49 +0330

    creating models

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    name = models.CharField(max_length=20, default=None, null=False)
+    note_text = models.TextField(max_length=300, null=True)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.name
+
+
+class List(models.Model):
+    todo = models.ForeignKey(Todo, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20, default=None, null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    list = models.ForeignKey(List, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20, default=None, null=False)
+    item_text = models.TextField(null=True, max_length=300)
+
+    def __str__(self):
+        return self.name
